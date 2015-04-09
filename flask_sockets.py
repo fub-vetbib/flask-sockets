338e15b84cb0cf6e5c7b1ce7a83ba68071577b2b
Rene Kijewski <rene.kijewski@fu-berlin.de>
2015-04-09 20:33:19 +0200

    Fix Flask.url_for, allow werkzeug route, cleanup
    * Fixes https://github.com/kennethreitz/flask-sockets/issues/6
    * Integrates https://github.com/kennethreitz/flask-sockets/pull/18
    * Integrates https://github.com/kennethreitz/flask-sockets/issues/19

--- flask_sockets.py
+++ flask_sockets.py
@@ -1,68 +1,110 @@
 # -*- coding: utf-8 -*-
 
-def log_request(self):
-    log = self.server.log
-    if log:
-        if hasattr(log, 'info'):
-            log.info(self.format_request() + '\n')
-        else:
-            log.write(self.format_request() + '\n')
+# Copyright (C) 2013-2014 Kenneth Reitz
+# Copyright (C) 2014      David K. Hess
+# Copyright (C) 2014      Jules Olleon
+# Copyright (C) 2015      René Kijewski / Freie Universität Berlin
+# License: MIT  http://opensource.org/licenses/MIT
+
+from flask.helpers import _endpoint_from_view_func
+from werkzeug.routing import Map, Rule, BuildError
+from werkzeug.exceptions import NotFound
 
 
-# Monkeys are made for freedom.
-try:
-    import gevent
-    from geventwebsocket.gunicorn.workers import GeventWebSocketWorker as Worker
-except ImportError:
-    pass
-
-if 'gevent' in locals():
-    # Freedom-Patch logger for Gunicorn.
-    if hasattr(gevent, 'pywsgi'):
-        gevent.pywsgi.WSGIHandler.log_request = log_request
+__all__ = ('Sockets',)
 
 
+class WsUrlAdapterWrapper(object):
+    def __init__(self, app_adapter, sockets_adapter):
+        self.__app_adapter = app_adapter
+        self.__sockets_adapter = sockets_adapter
 
-class SocketMiddleware(object):
+    def build(self, endpoint, values=None, method=None, force_external=False, append_unknown=True):
+        try:
+            return 'ws' + self.__sockets_adapter.build(
+                endpoint=endpoint,
+                values=values,
+                method=None,
+                force_external=True,
+                append_unknown=append_unknown,
+            )[4:]
+        except BuildError:
+            return self.__app_adapter.build(
+                endpoint=endpoint,
+                values=values,
+                method=method,
+                force_external=force_external,
+                append_unknown=append_unknown,
+            )
 
-    def __init__(self, wsgi_app, socket):
-        self.ws = socket
-        self.app = wsgi_app
-
-    def __call__(self, environ, start_response):
-        path = environ['PATH_INFO']
-
-        if path in self.ws.url_map:
-            handler = self.ws.url_map[path]
-            environment = environ['wsgi.websocket']
-
-            handler(environment)
-            return []
-        else:
-            return self.app(environ, start_response)
+    def __getattr__(self, attr):
+        fun = getattr(self.__app_adapter, attr)
+        setattr(self, attr, fun)
+        return fun
 
 
 class Sockets(object):
-
     def __init__(self, app=None):
-        self.url_map = {}
+        self.url_map = Map()
+        self.view_functions = {}
         if app:
             self.init_app(app)
 
+    def __create_url_adapter(self, url_map, request):
+        if request is not None:
+            return url_map.bind_to_environ(
+                request.environ,
+                server_name=self.app.config['SERVER_NAME']
+            )
+        elif self.app.config['SERVER_NAME'] is not None:
+            return url_map.bind(
+                self.app.config['SERVER_NAME'],
+                script_name=self.app.config['APPLICATION_ROOT'] or '/',
+                url_scheme=self.app.config['PREFERRED_URL_SCHEME']
+            )
+
+    def create_url_adapter(self, request):
+        adapter_for_app = self.__create_url_adapter(self.app.url_map, request)
+        adapter_for_sockets = self.__create_url_adapter(self.url_map, request)
+        return WsUrlAdapterWrapper(adapter_for_app, adapter_for_sockets)
+
     def init_app(self, app):
-        app.wsgi_app = SocketMiddleware(app.wsgi_app, self)
+        self.app = app
+        self.app_wsgi_app = app.wsgi_app
+
+        app.wsgi_app = self.wsgi_app
+        app.create_url_adapter = self.create_url_adapter
 
     def route(self, rule, **options):
-
         def decorator(f):
             endpoint = options.pop('endpoint', None)
             self.add_url_rule(rule, endpoint, f, **options)
             return f
         return decorator
 
-    def add_url_rule(self, rule, _, f, **options):
-        self.url_map[rule] = f
+    def add_url_rule(self, rule, endpoint, f, **options):
+        if endpoint is None:
+            endpoint = _endpoint_from_view_func(f)
 
-# CLI sugar.
-if 'Worker' in locals():
-    worker = Worker
+        methods = options.pop('methods', None)
+        options.setdefault('defaults', {}).setdefault('ws', None)
+
+        self.url_map.add(Rule(rule, endpoint=endpoint, **options))
+        self.view_functions[endpoint] = f
+
+        if methods is None:
+            methods = []
+        self.app.add_url_rule(rule, endpoint, f, methods=methods, **options)
+
+    def wsgi_app(self, environ, start_response):
+        if environ.get('HTTP_UPGRADE', '').lower() != 'websocket':
+            return self.app_wsgi_app(environ, start_response)
+
+        endpoint, values = self.url_map.bind_to_environ(environ).match()
+        view_function = self.view_functions[endpoint]
+        values['ws'] = environ['wsgi.websocket']
+
+        with self.app.app_context():
+            with self.app.request_context(environ):
+                view_function(**values)
+                return []
